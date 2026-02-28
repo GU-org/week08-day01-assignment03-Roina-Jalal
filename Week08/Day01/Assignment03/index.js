37c1224890b7c13768b287219ce7134bb0a2dfa3
Roina Shahidi <roinashahidi2@gmail.com>
2026-02-28 10:34:51 +0430

    bmi assignment

--- Week08/Day01/Assignment03/index.js
+++ Week08/Day01/Assignment03/index.js
@@ -1,11 +1,12 @@
-function bmiCalc(w,h){
-    let bmi
+function bmiCalc(w, h) {
+  let bmi = w / h ** 2;
 
-    return bmi;
-
+  return bmi;
 }
 
-var weight=prompt("Please enter you weight in kg");
-var height=prompt("Please enter your height in m");
-let bmi=bmiCalc(weight,height);
-alert("Your bmi is "+bmi);
+// BMI=height2weight​
+
+var weight = prompt("Please enter you weight in kg");
+var height = prompt("Please enter your height in m");
+let bmi = bmiCalc(weight, height);
+alert("Your bmi is " + bmi);
