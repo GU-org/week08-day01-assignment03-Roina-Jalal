a1ef2fc5a2059af1ba47008f1a5fcd7a0dfd123f
Roina Shahidi <roinashahidi2@gmail.com>
2026-02-28 10:18:53 +0430

    week 8 assignment.

--- Week08/Day01/Assignment02/index.js
+++ Week08/Day01/Assignment02/index.js
@@ -1,14 +1,14 @@
 function lifeInWeeks(age) {
-    
-/************Don't change the code above************/    
-    
-    //Write your code here.
-   
-    
-    
-    
-    
-    
-/*************Don't change the code below**********/
-}
+  /************Don't change the code above************/
 
+  //Write your code here.
+  let age = prompt("What is your current age?");
+  let live = prompt("How many years do you want to live?");
+  let years = live - age;
+  let days = years * 365;
+  let weeks = years * 52;
+  let months = years * 12;
+  alert(`You have ${days} days, ${weeks} weeks, and ${months} months left.`);
+
+  /*************Don't change the code below**********/
+}
